a2ea2c56eccde7ccd4eb714c8d689139cb9f3c73
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-03-18 12:19:28 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6285712420674
-│┝‷✧ *Dana:* 6285712420674
-│┝‷✧ *Linkaja:* 6285712420674
-│┝‷✧ *Gopay:* 6285712420674
-│┝‷✧ *Ovo:* 6285712420674
+│┝‷✧ *Pulsa:* 60147366955
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6285712420674?text=kak+mau+donasi
+│wa.me/6014366955?text=put+mau+donasi
 ╰──────────···───╮
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Owner', '.owner')

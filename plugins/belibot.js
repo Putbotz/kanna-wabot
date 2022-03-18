4ec0ef847db14fd8c289c05b5ded7c9969eb29b8
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-03-18 12:03:06 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,18 +1,17 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
-	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭═══ *〘 SEWA 〙*
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `╭═══ *〘 SEWA 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ Sewa = _5k/grup (1 minggu)_
-║┊ ⌲ Sewa = _15k/grup (1 bulan)_
-║┊ ⌲ Sewa = _20k/grup (2 bulan)_
-║┊ ⌲ Sewa = _35k/grup (8 bulan)_                                                                      ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
+║┊ ⌲ Sewa = _Rm5/grup (1 minggu)_
+║┊ ⌲ Sewa = _Rm15/grup (1 bulan)_
+║┊ ⌲ Sewa = _Rm20/grup (2 bulan)_
+║┊ ⌲ Sewa = _Rn30/grup (8 bulan)_                                                                      ║┊ ⌲ Sewa = _Rm50/grup (12 bulan)_
 ╰═ ┅ ═══════
 ╭═══ *〘 PAKET HEMAT 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ _20k (Sewa 1 bulan, premium 1 Bulan)_
-║┊ ⌲ _30k (Sewa 2 bulan, premium 1.5 bulan)_
-║┊ ⌲  _45k (8 Bulan sewa, 5 Bulan premium)_
-║┊ ⌲  _65k (all permanent)_
+║┊ ⌲ _Rm20 (Sewa 1 bulan, premium 1 Bulan)_
+║┊ ⌲ _Rm30 (Sewa 2 bulan, premium 1.5 bulan)_
+║┊ ⌲  _Rm45 (8 Bulan sewa, 5 Bulan premium)_
+║┊ ⌲  _Rm50 (all permanent)_
 ╰═ ┅ ═══════
 
 ༅ _*KEUNTUNGAN SEWABOT*_
@@ -28,16 +27,26 @@ let handler = async (m, { conn }) => {
 
 ◪ *Jika Minat Hubungi owner kami*
 ◪ *Atau Masuk Group Official Bot*
-_https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT_
+_https://chat.whatsapp.com/EGwXft3tf1z3VhKfx0W379_
 
 ▌│█║▌║▌║║▌║▌║█│▌
-`.trim(), `${wm}`, 'Payment', '#payment', 'Owner', '#Nowner', m)
-}
+`.trim()
 
-handler.help = ['sewa', 'sewabot']
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*✧──────────···──────────✧*\n                   Putbotz Ganteng`, 'status@broadcast')
+}
+handler.help = ['sewa']
 handler.tags = ['info']
-handler.command = /^sewa(bot)?$/i
+handler.command = /^(sewa)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
 
 module.exports = handler
-
-let wm = global.botwm
